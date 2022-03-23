bee7f081f0ce6dd237777a600e44a571ee45b4e8
shpendnila <shpendnila7@gmail.com>
2022-03-23 23:35:06 +0100

    added subscriber that listens for messages being published to multiple channels

---
+++ task-controller/src/main.py
@@ -0,0 +1,14 @@
+from src.config import configuration
+from src.utils import get_redis_client, listen_to_channels
+
+
+def main():
+    redis_client = get_redis_client()
+    sub = redis_client.pubsub().subscribe(
+        configuration.CHANNELS
+    )
+    listen_to_channels(sub)
+
+
+if __name__ == '__main__':
+    main()
--- task-controller/src/utils.py
+++ task-controller/src/utils.py
@@ -1,15 +1,30 @@
 from src.logger import get_logger
 from src.config import configuration
 
+import time
+import json
 import redis
 
-
 logger = get_logger(log_level=configuration.LOG_LEVEL)
+redis_host = configuration.HOST
+redis_port = configuration.PORT
 
 
 def get_redis_client():
+    logger.debug(f"connecting to redis on {redis_host}:{redis_port}")
     return redis.Redis(
-        host=configuration.HOST,
-        port=configuration.PORT
+        host=redis_host,
+        port=redis_port,
     )
 
+
+def listen_to_channels(subscriber):
+    logger.info("Listening for messages")
+    while True:
+        message = subscriber.get_message()
+        if message and message['type'] == "message":
+            channel = message['channel']
+            data = json.loads(message['data'])
+            logger.info(f"channel: {channel}")
+            logger.info(f"data: {data}")
+        time.sleep(0.1)

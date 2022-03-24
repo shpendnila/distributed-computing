4626d5afe703d4ad8ef190a935d0cbf5e0e256af
shpendnila <shpendnila7@gmail.com>
2022-03-24 13:39:53 +0100

    conver yaml format to list of channels

--- task-controller/src/config.py
+++ task-controller/src/config.py
@@ -1,12 +1,16 @@
 from functools import lru_cache
 import os
 
+from yaml import full_load
+
 
 class Config(object):
     # redis config
     HOST = os.getenv("HOST", "localhost")
     PORT = os.getenv("PORT", 6379)
-    CHANNELS = os.getenv("CHANNELS", ["test-channel"])
+    CHANNELS = full_load(
+        os.getenv("CHANNELS", ["test-channel"])
+    )
 
     LOG_LEVEL = int(os.getenv("LOG_LEVEL", 10))
 

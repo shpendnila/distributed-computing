f135e19ea97523c268c98fd669086cbea9e85c78
shpendnila <shpendnila7@gmail.com>
2022-03-31 10:09:55 +0200

    added main logic to run the uviron server

---
+++ task-service/src/main.py
@@ -0,0 +1,23 @@
+import logging
+
+import uvicorn
+from fastapi import FastAPI
+from fastapi.responses import ORJSONResponse
+
+from api.v1 import task
+
+app = FastAPI(
+    docs_url='/api/openapi',
+    openapi_url='/api/openapi.json',
+    default_response_class=ORJSONResponse
+)
+
+app.include_router(task.router, prefix='/v1/task', tags=['task'])
+
+if __name__ == '__main__':
+    uvicorn.run(
+        'main:app',
+        host='0.0.0.0',
+        port=8000,
+        log_level=logging.DEBUG,
+    )

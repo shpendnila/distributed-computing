60e9e24462813d8b5801c87be8f6579aff8e0c61
shpendnila <shpendnila7@gmail.com>
2022-03-31 20:18:17 +0200

    added redis to run on startup and log config

--- task-service/src/main.py
+++ task-service/src/main.py
@@ -1,16 +1,29 @@
 import logging
 
+import aioredis
 import uvicorn
+
 from fastapi import FastAPI
 from fastapi.responses import ORJSONResponse
 
 from src.api.v1 import task
+from src.core import config
+from src.core.logger import LOGGING
+from src.db import redis
 
 app = FastAPI(
     docs_url='/api/openapi',
     openapi_url='/api/openapi.json',
     default_response_class=ORJSONResponse
 )
+
+
+@app.on_event('startup')
+async def startup():
+    redis.redis = await aioredis.Redis(
+        host=config.REDIS_HOST,
+        port=config.REDIS_PORT
+    )
 
 app.include_router(task.router, prefix='/v1/task', tags=['task'])
 
@@ -19,5 +32,6 @@ if __name__ == '__main__':
         'src.main:app',
         host='0.0.0.0',
         port=8000,
+        log_config=LOGGING,
         log_level=logging.DEBUG,
     )

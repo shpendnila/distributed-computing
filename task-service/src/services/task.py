beed6546bc93834d7ea3be974ba0c204260b4cf5
shpendnila <shpendnila7@gmail.com>
2022-04-01 00:17:54 +0200

    task service retrieves the key where the data is stored

--- task-service/src/services/task.py
+++ task-service/src/services/task.py
@@ -4,6 +4,7 @@ from typing import Optional
 from aioredis import Redis
 from fastapi import Depends
 from dataclasses import dataclass, field
+from uuid import uuid4
 
 from src.db.redis import get_redis
 from src.models.task import Task
@@ -14,27 +15,30 @@ from src.core.config import CHANNEL
 class TaskService:
     redis: Redis = field(default_factory=Redis)
 
-    async def publish_task_config(self, task: Task) -> str:
-        if not task:
-            return "Task is empty"
+    async def publish_task_config(self, task: Task, data_key:str):
+        if data_key:
+            task.env_vars = {"data_key": data_key}
         await self._publish_to_channel(task)
-        return f"task config was published into {CHANNEL}"
 
-    async def store_task_data_to_cache(self, task: Task) -> Optional[str]:
-        if not task:
+    async def store_task_data_to_cache(self, task_data: str) -> Optional[str]:
+        if task_data == '{"data":null}':
             return
-        await self._put_task_data_to_cache(task)
-        return str(task.uid)[:8]
+        return await self._put_task_data_to_cache(task_data)
 
-    async def _put_task_data_to_cache(self, task: Task):
+    async def _put_task_data_to_cache(self, task_data: str) -> str:
+        data_key = str(uuid4())[:8]
         await self.redis.set(
-            str(task.uid)[:8], task.json(), ex=120
+            data_key, task_data, ex=120
         )
+        return data_key
 
     async def _publish_to_channel(self, task: Task):
         await self.redis.publish(
             CHANNEL,
-            task.json()
+            task.json(
+                exclude_none=True,
+                exclude={"data"},
+            )
         )
 
 

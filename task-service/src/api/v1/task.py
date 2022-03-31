e4004b2cfbb31808b1d2096e07f6ffc28b72e9ee
shpendnila <shpendnila7@gmail.com>
2022-04-01 00:15:15 +0200

    create_task api is able to store data into redis if its received from the payload

--- task-service/src/api/v1/task.py
+++ task-service/src/api/v1/task.py
@@ -4,16 +4,23 @@ from fastapi import APIRouter, Depends
 
 router = APIRouter()
 
+
 @router.get("/health")
 async def get_health() -> dict:
     return {"message": "ok"}
 
 
 @router.post("/create_task")
-async def publish_task_info(task: Task, task_service: TaskService = Depends(get_task_service)):
-    task_key = await task_service.publish_task_config(task)
+async def publish_task_info(
+        task: Task,
+        task_service: TaskService = Depends(get_task_service)
+) -> dict:
+    data_key = await task_service.store_task_data_to_cache(
+        task.json(include={"data"})
+    )
+    await task_service.publish_task_config(task, data_key)
     return {
         "status": "success",
-        "task": task.dict(),
-        "task_key": task_key,
+        "task_name": task.name,
+        "data_key": data_key,
     }

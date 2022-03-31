40598aaafccfe0fc413fc329a7c83f43f13cf47a
shpendnila <shpendnila7@gmail.com>
2022-03-31 20:17:21 +0200

    added create_task api

--- task-service/src/api/v1/task.py
+++ task-service/src/api/v1/task.py
@@ -1,8 +1,19 @@
-from fastapi import APIRouter
+from src.models.task import Task
+from src.services.task import TaskService, get_task_service
+from fastapi import APIRouter, Depends
 
 router = APIRouter()
-
 
 @router.get("/health")
 async def get_health() -> dict:
     return {"message": "ok"}
+
+
+@router.post("/create_task")
+async def publish_task_info(task: Task, task_service: TaskService = Depends(get_task_service)):
+    task_key = await task_service.publish_task_config(task)
+    return {
+        "status": "success",
+        "task": task.dict(),
+        "task_key": task_key,
+    }
